e35ebc898fe6a346618f6b21aeef7979e495ec4a
rnweerasooriya@outlook.com <rnweerasooriya@outlook.com>
2024-12-09 12:06:07 +0530

    december 09 12 pm commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: false,
-  timeout:0,
+  timeout:60000,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -33,7 +33,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: false,
-    video:'off',
+    video:'on',
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
