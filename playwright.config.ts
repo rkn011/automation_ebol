429d49ab49fa99acb2207d22d9e9e3e6009d7302
rkn011 <99468982+rkn011@users.noreply.github.com>
2024-12-10 15:12:56 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: false,
-  timeout:6000,
+  timeout:0,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
